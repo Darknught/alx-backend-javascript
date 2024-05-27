363798e643aa58e1068da44770f7b1edd91ab89d
Darknught <hassino66@yahoo.com>
2024-05-27 19:04:44 +0000

    creating Iterator Object

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,24 @@
+export default function createIteratorObject(report) {
+  const allEmployees = report.allEmployees;
+  const employees = [];
+
+  // Flatten the employees into a single array
+  for (const department of Object.values(allEmployees)) {
+    employees.push(...department);
+  }
+
+  // Return an iterator
+  return {
+    [Symbol.iterator]() {
+      let index = 0;
+      return {
+        next() {
+          if (index < employees.length) {
+            return { value: employees[index++], done: false };
+          }
+          return { done: true };
+        }
+      };
+    }
+  };
+}
