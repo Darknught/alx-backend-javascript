555c176d92b288bbd3bed4cd5f8ac6ed47d8b9f9
Darknught <hassino66@yahoo.com>
2024-05-27 19:42:08 +0000

    fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  const _task = false;
-  const _task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    (function taskFunction() {
+    {
       const task = true;
       const task2 = false;
-    }());
+    }
   }
-  return [_task, _task2];
+  return [task, task2];
 }
