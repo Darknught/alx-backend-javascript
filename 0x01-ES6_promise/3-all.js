c4f175942d10e8a5e97791b9011f7f6a9495fc91
Darknught <hassino66@yahoo.com>
2024-05-28 19:03:02 +0000

    Function that handles signup details

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
-      console.log(`${userResponse.firstName} ${userResponse.lastName}`);
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
