2ed23fc4390a25ac5cc81ac1fe524e91a6a19eec
Darknught <hassino66@yahoo.com>
2024-05-29 10:38:22 +0000

    code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -8,7 +8,7 @@ import { uploadPhoto } from './5-photo-reject';
  * @param {string} firstName - The user's first name.
  * @param {string} lastName - The user's last name.
  * @param {string} fileName - The name of the photo file to be uploaded.
- * @returns {Promise<Array<{ status: string, value: any }>>} - An array of objects with the status and value/error of each promise.
+ * @returns {Promise<Array<{ status: string, value: any }>>} - An array of objects with status
  */
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
