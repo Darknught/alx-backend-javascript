5d2f36e621f2b639a67ecfc2d082276bfd7678f3
Darknught <hassino66@yahoo.com>
2024-05-30 13:11:21 +0000

    code

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,9 @@
 // A Function that implements a class with attributes, getter and setter
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this.validateString(name, 'name');
-    this._length = this.validateNumber(length, 'length');
-    this._students = this.validateArray(students, 'students');
+    this._name = HolbertonCourse.validateString(name, 'name');
+    this._length = HolbertonCourse.validateNumber(length, 'length');
+    this._students = HolbertonCourse.validateArray(students, 'students');
   }
 
   // Getters
@@ -21,33 +21,33 @@ export default class HolbertonCourse {
 
   // Setters
   set name(name) {
-    this._name = this.validateString(name, 'name');
+    this._name = HolbertonCourse.validateString(name, 'name');
   }
 
   set length(length) {
-    this._length = this.validateNumber(length, 'length');
+    this._length = HolbertonCourse.validateNumber(length, 'length');
   }
 
   set students(students) {
-    this._students = this.validateArray(students, 'students');
+    this._students = HolbertonCourse.validateArray(students, 'students');
   }
 
   // Validation methods
-  validateString(value, name) {
+  static validateString(value, name) {
     if (typeof value !== 'string') {
       throw new TypeError(`${name} must be a string`);
     }
     return value;
   }
 
-  validateNumber(value, name) {
+  static validateNumber(value, name) {
     if (typeof value !== 'number') {
       throw new TypeError(`${name} must be a number`);
     }
     return value;
   }
 
-  validateArray(value, name) {
+  static validateArray(value, name) {
     if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
       throw new TypeError(`${name} must be an array of strings`);
     }
