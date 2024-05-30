b9b4f5b8993d9314dbfe5c75c40f0bdbc3ce7955
Darknught <hassino66@yahoo.com>
2024-05-30 16:18:17 +0000

    code

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+// A function that implements a class Pricing imported from Currency
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getters
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._amount;
+  }
+
+  // Setters
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
