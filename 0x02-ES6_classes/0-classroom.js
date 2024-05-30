421bfb8587f0edc8ee9b4a1a37246c11a63b07c3
Darknught <hassino66@yahoo.com>
2024-05-30 11:45:27 +0000

    code

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,6 @@
 // A function that implement a class
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
