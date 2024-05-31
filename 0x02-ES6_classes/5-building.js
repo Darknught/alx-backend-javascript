37c15adcb59cb729e3fcaf81437b1272b5587e89
Darknught <hassino66@yahoo.com>
2024-05-31 17:00:38 +0000

    code

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,10 @@
 // A function that implements a class Building
 export default class Building {
   constructor(sqft) {
-    if (new.target !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+    if (
+      new.target !== Building &&
+      this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage
+    ) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
