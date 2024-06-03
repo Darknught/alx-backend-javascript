b3f5512854cef96a5cc418cb06af96751429d528
Darknught <hassino66@yahoo.com>
2024-06-03 18:40:28 +0000

    fixed an eslint issue

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,13 @@
 export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string' || set.size === 0) {
-    return "";
+    return '';
   }
 
-  const filteredValues = [...set].filter(value => {
+  const filteredValues = [...set].filter((value) => {
     if (typeof value === 'string') {
       return value.startsWith(startString);
     }
     return false;
   });
-  return filteredValues.join("-");
+  return filteredValues.join('-');
 }
