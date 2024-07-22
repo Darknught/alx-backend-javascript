6a675bf8df73f22343dbd9f1025c9b2df255f55b
Darknught <hassino66@yahoo.com>
2024-07-22 19:21:23 +0000

    code

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,6 @@
+// A Function that prints to the STDOUT
+function displayMessage(message) {
+        console.log(message);
+}
+// Export the function to make it available for import in other files
+module.exports = displayMessage;
