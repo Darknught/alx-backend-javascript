9f9e0ffe61bce08819760d754b35ada6d5594eb7
Darknught <hassino66@yahoo.com>
2024-07-23 06:02:48 +0000

    correction

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 // A Function that prints to the STDOUT
-const displayMessage(message) {
-    console.log(message);
-}
-// Export the function to make it available for import in other files
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
 module.exports = displayMessage;
